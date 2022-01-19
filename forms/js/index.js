e8912d49d082b63878cb6e0a84dbd992a2406039
Jhoctan Teixeira <rm88206@fiap.com.br>
2022-01-19 17:11:37 -0300

    Dois tipos de form um codificador e um de usuario criados

--- forms/js/index.js
+++ forms/js/index.js
@@ -11,3 +11,16 @@ function Enviar() {
         alert('Obrigado sr()a ' + nome.value + 'os seus dados foram encaminhados com sucesso')
     }
 }
+function Mudarestado(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "none")
+        document.getElementById(el).style.display = 'block';
+    else
+        document.getElementById(el).style.display = 'none';
+}
+var string = "DevPleno"
+var emBase64 = btoa(string)
+console.log(emBase64)
+
+var deBase64 = atob(emBase64)
+console.log(deBase64)
