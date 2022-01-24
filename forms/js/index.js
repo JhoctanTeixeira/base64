9377870c16e7c34983d35272833cec4cb6242172
Jhoctan Teixeira <rm88206@fiap.com.br>
2022-01-24 11:21:30 -0300

    base64 coding e ecoding completo

--- forms/js/index.js
+++ forms/js/index.js
@@ -23,15 +23,50 @@ function Mudarestado(el) {
 }
 
 // fim Mostrar e esconder buttons 
-$(document).ready(function(){
+// $(document).ready(function(){
 
-    $('#select').on('change', function(){
-        var selectValor = '#'+$(this).val();
+//     $('#select').on('change', function(){
+//         var selectValor = '#'+$(this).val();
 
-        $('#pai').children('div').hide();
-        $('#pai').children(selectValor).show();
-    });
-});
+//         $('#pai').children('div').hide();
+//         $('#pai').children(selectValor).show();
+//     });
+// });
 // Cesar 
 
 // Fim Cesar
+
+const st = {};
+
+st.flap = document.querySelector('#flap');
+st.toggle = document.querySelector('.toggle');
+
+st.choice1 = document.querySelector('#choice1');
+st.choice2 = document.querySelector('#choice2');
+
+st.flap.addEventListener('transitionend', () => {
+
+    if (st.choice1.checked) {
+        st.toggle.style.transform = 'rotateY(-15deg)';
+        setTimeout(() => st.toggle.style.transform = '', 400);
+    } else {
+        st.toggle.style.transform = 'rotateY(15deg)';
+        setTimeout(() => st.toggle.style.transform = '', 400);
+    }
+
+})
+
+st.clickHandler = (e) => {
+
+    if (e.target.tagName === 'LABEL') {
+        setTimeout(() => {
+            st.flap.children[0].textContent = e.target.textContent;
+        }, 250);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    st.flap.children[0].textContent = st.choice2.nextElementSibling.textContent;
+});
+
+document.addEventListener('click', (e) => st.clickHandler(e));
